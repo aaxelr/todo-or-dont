ef83901118ea56a88cec3057dc1ea450c5458389
aaxelr <antonio.axelsson@gmail.com>
2023-01-19 23:30:24 +0100

    Initialize prisma client

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,5 +1,8 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
+
+import type { PrismaClient } from '@prisma/client';
+
 // and what to do when importing types
 declare global {
 	namespace App {
@@ -8,6 +11,9 @@ declare global {
 		// interface PageData {}
 		// interface Platform {}
 	}
+	// store prisma client as a global variable while running in dev mode to 
+	// prevent the creation of a new instance of the prisma client at every HMR.
+	var prisma: PrismaClient;
 }
 
 export {};
