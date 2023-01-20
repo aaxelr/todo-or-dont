5673f3702e72ca81b396aa1d1e82f74421169b61
aaxelr <antonio.axelsson@gmail.com>
2023-01-20 12:50:34 +0100

    Create +layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import Header from '$lib/components/Header.svelte';
+</script>
+
+<Header />
+<main class="container">
+	<slot />
+</main>
+
+<style>
+	main {
+		margin-top: 150px;
+	}
+
+	@media (max-width: 575px) {
+		main {
+			margin-top: 200px;
+		}
+	}
+</style>
