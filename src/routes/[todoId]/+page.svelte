2ea141ac53b1a31c835d1d2ef81355f95da2e679
aaxelr <antonio.axelsson@gmail.com>
2023-01-24 16:07:20 +0100

    Add dynamic route for editing Todo

---
+++ src/routes/[todoId]/+page.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+	$: ({ todo } = data);
+</script>
+
+<svelte:head>
+	<title>Editing: {todo.title}</title>
+</svelte:head>
+
+<form action="?/updateTodo" method="POST">
+	<h2>Editing: {todo.title}</h2>
+	<label>
+		Title:
+		<input type="text" name="title" value={todo.title} />
+	</label>
+	<label>
+		Content:
+		<textarea name="content" cols="20" rows="6" value={todo.content} />
+	</label>
+	<label>
+		Todon't?
+		<input type="checkbox" name="todont" checked={todo.todont} />
+	</label>
+	<button type="submit">Update</button>
+</form>
