4c181984aa09b76226f1e9ac7c9ae279a35a71e2
aaxelr <antonio.axelsson@gmail.com>
2023-01-24 15:22:54 +0100

    Styling

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -10,21 +10,26 @@
 </script>
 
 <svelte:window on:scroll={handleScroll} />
+<svelte:head>
+	<title>To do, or not to do</title>
+</svelte:head>
 
 <header class:visible>
 	<nav>
 		<ul>
-			<li>
-				To do, or not to do <svg
-					xmlns="http://www.w3.org/2000/svg"
-					width="32"
-					height="32"
-					viewBox="0 0 24 24"
-					><path
-						fill="currentColor"
-						d="M12 2C5.505 2 2 6.637 2 11c0 2.129 1.009 3.979 3 5.508V21h3v-3h2v3h4v-3h2v3h3v-4.493c1.991-1.528 3-3.379 3-5.507c0-4.363-3.505-9-10-9zM8 13c-1.121 0-2-1.098-2-2.5S6.879 8 8 8s2 1.098 2 2.5S9.121 13 8 13zm8 0c-1.121 0-2-1.098-2-2.5S14.879 8 16 8s2 1.098 2 2.5s-.879 2.5-2 2.5z"
-					/></svg
-				>
+			<li class="title">
+				<a href="/">
+					To do, or not to do <svg
+						xmlns="http://www.w3.org/2000/svg"
+						width="32"
+						height="32"
+						viewBox="0 0 24 24"
+						><path
+							fill="currentColor"
+							d="M12 2C5.505 2 2 6.637 2 11c0 2.129 1.009 3.979 3 5.508V21h3v-3h2v3h4v-3h2v3h3v-4.493c1.991-1.528 3-3.379 3-5.507c0-4.363-3.505-9-10-9zM8 13c-1.121 0-2-1.098-2-2.5S6.879 8 8 8s2 1.098 2 2.5S9.121 13 8 13zm8 0c-1.121 0-2-1.098-2-2.5S14.879 8 16 8s2 1.098 2 2.5s-.879 2.5-2 2.5z"
+						/></svg
+					>
+				</a>
 			</li>
 		</ul>
 		<ul class="nav-links">
@@ -36,6 +41,15 @@
 </header>
 
 <style>
+	.title {
+		font-size: 2rem;
+		font-weight: 600;
+	}
+
+	.title a {
+		margin: 0;
+	}
+
 	header {
 		height: 100px;
 		position: fixed;
